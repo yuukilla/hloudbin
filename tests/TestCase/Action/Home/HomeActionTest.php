50cdef592011501660350b4ee6b1a887d20a660e
Gabriels <gabripub@gmail.com>
2022-11-17 08:36:17 +0200

    fix test

--- tests/TestCase/Action/Home/HomeActionTest.php
+++ tests/TestCase/Action/Home/HomeActionTest.php
@@ -21,7 +21,7 @@ class HomeActionTest extends TestCase
         $response = $this->app->handle($request);
 
         $this->assertSame(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
-        $this->assertResponseContains($response, 'Welcome!');
+        $this->assertResponseContains($response, 'hloudBin');
     }
 
     public function testPageNotFound(): void
